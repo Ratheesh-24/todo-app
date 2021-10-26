3e96b1d70a9d5377433728a066ade0f7cc2daed1
Ratheesh K <ratheeshkarnan1999@gmail.com>
2021-10-25 22:14:28 -0700

    codes are added

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <ColorModeScript initialColorMode="light" />
+      <App />
+    </ChakraProvider>    
   </React.StrictMode>,
   document.getElementById('root')
 );
